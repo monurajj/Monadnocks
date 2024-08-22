ba87cc641f3e4da430f358241610e96aea919350
Monu Rajj <monu2feb2004.com>
2024-08-22 14:48:17 +0530

    initial change

---
+++ src/components/navbar.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import Image from "next/image";
+import logoImage from "../assets/logoImage.jpeg";
+
+function NavBar() {
+  return (
+    <nav className="sticky z-[100] inset-x-0 top-0 w-full border-b border-gray-200 bg-white/75 backdrop-blur-lg transition-all flex items-center justify-center">
+      <div className="flex items-center ml-4">
+        <Image
+          className="w-12 h-12 object-cover rounded-full"
+          src={logoImage}
+          alt="Logo Image"
+        />
+      </div>
+
+      <div>
+        <ul>
+            <li>
+                
+            </li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
+
+export default NavBar;
---
+++ src/pages/homepage.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import NavBar from '@/components/navbar';
+function HomePage() {
+    return (
+        <div>
+            <NavBar/>
+            {/* body */}
+
+            {/* row 1 */}
+            <div>
+                
+            </div>
+
+            {/* row 2 */}
+            <div></div>
+
+            {/* row 3 */}
+            <div></div>
+
+            {/* row 4 */}
+            <div></div>
+
+            {/* row 5 */}
+            <div></div>
+
+        </div>
+    );
+}
+
+export default HomePage;
